efcdc618a648cd3434f9836504e1a60be43bdf56
santosh10499 <115284357+santosh10499@users.noreply.github.com>
2022-12-08 17:00:27 +0530

    Add files via upload

---
+++ hemberger.js
@@ -0,0 +1,9 @@
+
+    let hemIcon=document.getElementById('hem');
+    let menuItem=document.querySelector('.navbar');
+
+    hemIcon.addEventListener('click',()=>
+    {
+      menuItem.classList.toggle('dis');
+    })
+
